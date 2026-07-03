4769868b1316ea64e05d1dacf740824baf929720
shawnxxxhu <shawnxxxhu@tencent.com>
2026-07-03 23:01:19 +0800

    refactor

--- tasks/train_with_ruler.py
+++
@@ -1,8 +0,0 @@
-from veomni.arguments import parse_args
-from veomni.trainer.text_trainer import TextTrainer, VeOmniArguments
-
-
-if __name__ == "__main__":
-    args = parse_args(VeOmniArguments)
-    trainer = TextTrainer(args)
-    trainer.train()
